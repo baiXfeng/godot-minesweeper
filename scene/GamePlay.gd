b9cde91d661db4f9b4e2bf84dc55339b8cad799c
baiXfeng <394811161@qq.com>
2023-06-12 13:33:22 +0800

    新的初始化逻辑

---
+++ scene/GamePlay.gd
@@ -0,0 +1,149 @@
+extends Node
+
+onready var ground_layer = $Background
+onready var item_layer = $ItemLayer
+onready var top_layer = $TopLayer
+
+var map_size:Vector2
+var mine_size:int
+var map_border:Rect2
+var select_point:Vector2
+
+const MINE_ID = 8
+
+const offsets_8 = [
+	Vector2(-1, -1),
+	Vector2(0, -1),
+	Vector2(1, -1),
+	Vector2(-1, 0),
+	Vector2(1, 0),
+	Vector2(-1, 1),
+	Vector2(0, 1),
+	Vector2(1, 1),
+]
+const offsets_4 = [
+	Vector2(0, -1),
+	Vector2(1, 0),
+	Vector2(0, 1),
+	Vector2(-1, 0),
+]
+
+func _ready():
+	randomize()
+	build_map(Vector2(10, 10), 5)
+
+func build_map(mapSize:Vector2, mineSize:int):
+	
+	map_size = mapSize
+	mine_size = mineSize
+	map_border = Rect2(Vector2.ZERO, mapSize)
+	
+	ground_layer.clear()
+	item_layer.clear()
+	top_layer.clear()
+	
+	var points = []
+	
+	for y in mapSize.y:
+		for x in mapSize.x:
+			var point = Vector2(x, y)
+			ground_layer.set_cellv(point, 0)
+			top_layer.set_cell(x, y, 0)
+			points.push_back(point)
+	
+	points.shuffle()
+	
+	for i in mineSize:
+		var point = points[i]
+		item_layer.set_cellv(point, MINE_ID)
+	
+	for i in mineSize:
+		var point = points[i]
+		_proc_mine_number(point)
+
+func _proc_mine_number(point:Vector2):
+	for p in offsets_8:
+		var next = point + p
+		if !map_border.has_point(next):
+			continue
+		_fill_number(next)
+
+func _fill_number(point:Vector2):
+	var number = 0
+	for p in offsets_8:
+		var next = point + p
+		if _is_mine_tile(item_layer.get_cellv(next)):
+			number += 1
+	if number >= 1:
+		item_layer.set_cellv(point, number-1)
+
+func _open_tile(point:Vector2):
+	var id = item_layer.get_cellv(point)
+	if _is_mine_tile(id):
+		# 游戏结束
+		print("game over.")
+		return
+	if _is_empty_tile(id):
+		# 展开邻接的所有空白块
+		_open_empty_tile(point)
+		return
+	for p in offsets_4:
+		var next = point + p
+		if !map_border.has_point(next):
+			continue
+		if _is_empty_tile(item_layer.get_cellv(next)):
+			# 展开邻接的所有空白块
+			_open_empty_tile(next)
+
+func _open_empty_tile(point:Vector2):
+
+	var check_list = [point]
+	var closed_list = []
+	
+	while !check_list.empty():
+		var current = check_list.pop_back()
+		if _is_empty_tile(item_layer.get_cellv(current)):
+			for p in offsets_8:
+				var next = current + p
+				if next in closed_list:
+					continue
+				if map_border.has_point(next):
+					check_list.push_back(next)
+		closed_list.push_back(current)
+	for point in closed_list:
+		top_layer.set_cellv(point, -1)
+
+func _is_number_tile(id:int):
+	return id >= 0 and id <= 7
+
+func _is_empty_tile(id:int):
+	return id == -1
+
+func _is_mine_tile(id:int):
+	return id == MINE_ID
+
+func _input(event):
+	var mouse = event as InputEventMouseButton
+	if mouse:
+		if mouse.button_index == BUTTON_RIGHT and mouse.pressed:
+			build_map(Vector2(10, 10), 5)
+			return
+		if mouse.button_index == BUTTON_LEFT:
+			var point = (mouse.position / 32).floor()
+			if _is_empty_tile(top_layer.get_cellv(point)):
+				return
+			if mouse.pressed:
+				if !map_border.has_point(point):
+					return
+				top_layer.set_cellv(point, 1)
+				select_point = point
+			else:
+				if point != select_point:
+					top_layer.set_cellv(select_point, 0)
+					return
+				top_layer.set_cellv(select_point, -1)
+				_open_tile(select_point)
+			return
+		if mouse.button_index == BUTTON_MIDDLE and mouse.pressed:
+			var point = (mouse.position / 32).floor()
+			top_layer.set_cellv(point, 0)
